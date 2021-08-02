07179c74f57c17a1a17b9c58addee656d4a3d5ea
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-08-02 14:24:13 +0530

    db connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
@@ -12,3 +14,11 @@ app.get("/", (req, res) => {
 app.listen((port), () => {
     console.log(`server up and running on port ${port}`);
 });
+
+mongoose.connect((process.env.MONGODB_URI), {useNewUrlParser: true, useUnifiedTopology:true} , (err) => {
+    if(!err){
+        console.log("Database connected");
+    }else{
+        console.log(err);
+    }
+})
